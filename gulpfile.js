8fab4d416d3dc34e53c0ffb0dace1c73e69fbc36
paul-frdev <pavelzadyrko@gmail.com>
2021-03-08 16:39:54 +0200

    added products, design, articles, video-popup, partners

--- gulpfile.js
+++ gulpfile.js
@@ -52,6 +52,7 @@ function scripts() {
   return src([
       'node_modules/jquery/dist/jquery.js',
       'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
       'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
       'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
       'app/js/main.js'
