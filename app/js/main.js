a4f1c60cebf5f45c6f170408be0d28a3e7ae0afc
paul-frdev <pavelzadyrko@gmail.com>
2021-03-24 18:02:46 +0200

    added page, login

--- app/js/main.js
+++ app/js/main.js
@@ -8,3 +8,17 @@ $(function () {
   });
 
 });
+
+// document.querySelector('#search').oninput = function () {
+//   let val = this.value.trim();
+//   let searchItems = document.querySelectorAll('.user-nav__search-input body');
+//   if (val != '') {
+//     searchItems.forEach(function (elem) {
+//       if (elem.innerText.search(val) == -1) {
+//         elem.classList.add('hide');
+//       } else {
+//         elem.classList.remove('hide');
+//       }
+//     });
+//   }
+// }
